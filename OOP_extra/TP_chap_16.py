1ec37cdf8e54f52defb3475d1114c479d3b4dbe4
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-03-07 17:48:54 +0100

    [OOP] add exercise TP_17

--- OOP_extra/TP_chap_16.py
+++ OOP_extra/TP_chap_16.py
@@ -1,31 +1,9 @@
-
-class Time:
-    """Represents the time of day.
-
-    attributes: hour, minutes, second
-    """
+import copy
+from time import Time
 
 
-time = Time()
-time.hour = 11
-time.minute = 59
-time.second = 30
-
-time2 = Time()
-time2.hour = 12
-time2.minute = 59
-time2.second = 30
-
-
-def print_time(time_objects):
-    print('%.2d:%.2d:%.2d' % (time_objects.hour, time_objects.minute, time_objects.second))
-
-
-def is_after(t1, t2):
-    return t1.hour > t2.hour
-
-
-print(is_after(time, time2))
+def print_time(time):
+    print('%.2d:%.2d:%.2d' % (time.hour, time.minute, time.second))
 
 
 def time_to_int(time):
@@ -46,6 +24,13 @@ def add_time(t1, t2):
     return int_to_time(seconds)
 
 
+def increment(time, seconds):
+    my_time = copy.deepcopy(time)  # create a copy of time object passed
+    tot_seconds = time_to_int(my_time) + seconds
+
+    return int_to_time(tot_seconds)
+
+
 # start time of a movie
 start = Time()
 start.hour = 9
@@ -60,4 +45,15 @@ duration.second = 0
 
 
 done = add_time(start, duration)
-print_time(done)
+# print_time(done)
+
+
+time = Time()
+time.hour = 11
+time.minute = 59
+time.second = 30
+
+time2 = Time()
+time2.hour = 12
+time2.minute = 59
+time2.second = 30
---
+++ OOP_extra/time.py
@@ -0,0 +1,29 @@
+from TP_chap_16 import int_to_time
+
+
+class Time:
+
+    def __init__(self):
+        self.hour = None
+        self.minute = None
+        self.second = None
+
+    def print_time(self):
+        print('%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second))
+
+    def time_to_int(self):
+        minutes = self.hour * 60 + self.minute
+        seconds = minutes * 60 + self.second
+        return seconds
+
+    def int_to_time(self, seconds):
+        self.minute, self.second = divmod(seconds, 60)
+        self.hour, self.minute = divmod(minute, 60)
+
+    def increment(self, seconds):
+        seconds += self.time_to_int()
+        return int_to_time(seconds)
+
+    def is_after(self, other):
+        return self.time_to_int() > other.time_to_int()
+
