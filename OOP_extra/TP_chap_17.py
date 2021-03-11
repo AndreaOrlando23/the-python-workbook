4682508643fe4c0612d4e889a2e75d67617b51fd
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-03-11 20:11:45 +0100

    [OOP] add dir TP_Cap_17

--- OOP_extra/TP_chap_17.py
+++ OOP_extra/TP_chap_17.py
@@ -1,4 +1,34 @@
+# from TP_chap_16 import int_to_time
 from time import Time
+
+class Time:
+
+    def __init__(self, hour=0, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+
+    def print_time(self):
+        print('%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second))
+
+    def time_to_int(self):
+        minutes = self.hour * 60 + self.minute
+        seconds = minutes * 60 + self.second
+        return seconds
+
+    def increment(self, seconds):
+        seconds += self.time_to_int()
+        return int_to_time(seconds)
+
+    def is_after(self, other):
+        return self.time_to_int() > other.time_to_int()
+
+
+def int_to_time(seconds):
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
 
 
 start = Time()
