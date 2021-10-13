3fd77ff02686c2877e57f7badaf3c95676a26758
AndreaOrlando23 <andrea.orlando87@gmail.com>
2021-10-13 15:38:54 +0200

    refactor: implement Making Change problem with float and euro exchange

--- Cap_1_Introduction/ex_13.py
+++ Cap_1_Introduction/ex_13.py
@@ -1,5 +1,45 @@
 # EXERCISE 13 : Making change
 
+CENTS_PER_TWO_EURO = 200
+CENTS_PER_ONE_EURO = 100
+HALF_EURO = 50
+TWENTY_CENTS = 20
+TEN_CENTS = 10
+FIVE_CENTS = 5
+ONE_CENTS = 1
+
+euro = float(input('Enter the number of euro: '))
+cents = euro * 100
+
+print(' {}  x  2.00 €'.format(int(cents // CENTS_PER_TWO_EURO)))
+cents = cents % CENTS_PER_TWO_EURO
+
+print(' {}  x  1.00 €'.format(int(cents // CENTS_PER_ONE_EURO)))
+cents = cents % CENTS_PER_ONE_EURO
+
+print(' {}  x  0.50 €'.format(int(cents // HALF_EURO)))
+cents = cents % HALF_EURO
+
+print(' {}  x  0.20 €'.format(int(cents // TWENTY_CENTS)))
+cents = cents % TWENTY_CENTS
+
+print(' {}  x  0.10 €'.format(int(cents // TEN_CENTS)))
+cents = cents % TEN_CENTS
+
+print(' {}  x  0.05 €'.format(int(cents // FIVE_CENTS)))
+cents = cents % FIVE_CENTS
+
+print(' {}  x  0.01 €'.format(int(cents // ONE_CENTS)))
+cents = cents % ONE_CENTS
+
+
+
+
+
+
+"""
+---BOOK VERSION---
+
 CENTS_PER_TOONIE = 200
 CENTS_PER_LOONIE = 100
 CENTS_PER_QUARTER = 15
@@ -28,5 +68,7 @@ cents = cents % CENTS_PER_DIME
 print(' ', cents // CENTS_PER_NICKEL, 'nickels')
 cents = cents % CENTS_PER_NICKEL
 
-print(' ', cents // CENTS_PER_PENNIE, 'pennies')
-cents = cents % CENTS_PER_PENNIE
+print(' ', cents, 'pennies')
+# print(' ', cents // CENTS_PER_PENNIE, 'pennies')
+# cents = cents % CENTS_PER_PENNIE
+"""
