78f7db48b7ade2696cbfda0a5cb1858eaac1bcfc
Andrea Orlando <andrea.orlando87@gmail.com>
2021-10-20 12:00:09 +0200

    refactor: implement function for managing caesar_cipher ex

--- Cap_3_Repetition/ex_73.py
+++ Cap_3_Repetition/ex_73.py
@@ -1,34 +1,46 @@
 # EXERCISE 73 : Caesar Cipher
 
-# Read the message and shift amount from the user
-message = input("Enter the message: ")
-shift = int(input("Enter the shift value: "))
 
+def caesar_cipher(message, shift):
 # Process each character to construct the encrypted (or decrypted) message
-new_message = ""
-for ch in message:
-    if "a" <= ch <= "z":
-        # Process a lowercase letter by determining its
-        # position in the alphabet (0-25), computing its
-        # new position, and adding it to the new message
-        pos = ord(ch) - ord("a")
-        pos = (pos + shift) % 26
-        new_char = chr(pos + ord("a"))
-        new_message += new_char
-    elif "A" <= ch <= "Z":
-        # Process an uppercase letter by determining its
-        # position in the alphabet (0-25), computing its
-        # new position, and adding it to the new message
-        pos = ord(ch) - ord("A")
-        pos = (pos + shift) % 26
-        new_char = chr(pos + ord("A"))
-        new_message += new_char
-    else:
-        # If the character is not a letter then copy it into the new message
-        new_message += ch
+    encrypted_message = ""
+    for ch in message:
+        if "a" <= ch <= "z":
+            # Process a lowercase letter by determining its
+            # position in the alphabet (0-25), computing its
+            # new position, and adding it to the new message
+            pos = ord(ch) - ord("a")
+            pos = (pos + shift) % 26
+            new_char = chr(pos + ord("a"))
+            encrypted_message += new_char
+        elif "A" <= ch <= "Z":
+            # Process an uppercase letter by determining its
+            # position in the alphabet (0-25), computing its
+            # new position, and adding it to the new message
+            pos = ord(ch) - ord("A")
+            pos = (pos + shift) % 26
+            new_char = chr(pos + ord("A"))
+            encrypted_message += new_char
+        else:
+            # If the character is not a letter then copy it into the new message
+            encrypted_message += ch
+    
+    return encrypted_message
 
-# Display the message
-print(f"The shifted message is: {new_message}")
+
+def main():
+    # Read the message and shift amount from the user
+    message = input("Enter the message: ")
+    shift = int(input("Enter the shift value: "))
+
+    encrypted_message = caesar_cipher(message, shift)
+
+    # Display the message
+    print(f"The shifted message is: {encrypted_message}")
+
+
+if __name__ == "__main__":
+    main()
 
 
 """
