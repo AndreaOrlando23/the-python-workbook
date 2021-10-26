4bb3631352d7cccbca6f1dbb405a86733c092860
Andrea Orlando <andrea.orlando87@gmail.com>
2021-10-26 16:41:23 +0200

    refactor: correct tab distance on output

--- Cap_3_Repetition/ex_65.py
+++ Cap_3_Repetition/ex_65.py
@@ -2,5 +2,5 @@
 
 for degrees_celsius in range(0, 101, 10):
     fahrenheit = (degrees_celsius * 9/5) + 32
-    print("Celsius\t-->\tFahrenheit")
-    print(degrees_celsius, "\t\t\t\t", fahrenheit)
+    print("Celsius --> Fahrenheit")
+    print(degrees_celsius, "\t\t", fahrenheit)
