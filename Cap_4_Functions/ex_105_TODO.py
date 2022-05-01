9fc96f7310720c2cabd4a4ef8a35b36eed088668
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2022-05-01 20:00:32 +0200

    feat: implement first solution ex105

--- Cap_4_Functions/ex_105_TODO.py
+++ Cap_4_Functions/ex_105_TODO.py
@@ -1,2 +1,32 @@
 # EXERCISE 105 : Arbitrary base conversions
 # TODO
+
+
+def base_check(base):
+    if 2 <= base <= 16:
+        return True
+    return False
+
+
+def numberToBase(n, b):
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    return digits[::-1]
+
+
+def main():
+    base = int(input("Chose the base (2 to 16): "))
+    if not base_check(base):
+        print("ERROR: Please chose a valid base")
+        main()
+    num = int(input("Enter a number: "))
+    print(numberToBase(num, base))
+
+if __name__ == '__main__':
+    main()
+
+
